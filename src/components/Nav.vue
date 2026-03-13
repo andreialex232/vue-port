606cc77f0e75eb8a2ffaced7d9a346b862fac9b4
andreialex232 <numauitnuintelegi@gmail.com>
2026-03-13 15:49:48 +0100

    test nav created

---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li>Language:</li>
+        </ul>
+    </nav>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
