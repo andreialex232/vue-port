d205cf4b44ffe54d78cff0baa934e3b366930af6
andreialex232 <numauitnuintelegi@gmail.com>
2026-03-15 03:17:18 +0100

    updated hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,13 +1,62 @@
 <script lang="ts" setup>
     import video from "@/assets/videos/ninety-dark.mp4";
+    import gsap from "gsap";
+    import { ScrollTrigger } from "gsap/ScrollTrigger";
+    import TextPlugin from "gsap/TextPlugin";
+    import { ref, onMounted } from "vue";
+    gsap.registerPlugin(TextPlugin); 
+    gsap.registerPlugin(ScrollTrigger);
+    const kindOfDev = ref(null);
+
+    onMounted(() => {
+        // Switching from creative to front end animation
+        const devKindTl = gsap.timeline({ repeat: -1, repeatDelay: 1 })
+        .to(kindOfDev.value, {
+            text: "Front End",
+            ease: "none",
+            duration: 1.2
+        })
+        .to({}, {
+            duration: 2
+        })
+        .to(kindOfDev.value, {
+            duration: 0.8,
+            text: "",
+            ease: "power2.inOut"
+        })
+        .to(kindOfDev.value, {
+            duration: 1.2,
+            text: "Creative",
+            ease: "none"
+        })
+        .to({}, {
+            duration: 2
+        })
+        .to(kindOfDev.value, {
+            duration: 0.8,
+            text: "",
+            ease: "power2.inOut"
+        })
+
+
+        // stops the animation if its not on the screen
+        ScrollTrigger.create({
+            trigger: ".video-container",
+            start: "top center",
+            onEnter: () => { devKindTl.play() },
+            onEnterBack: () => { devKindTl.play() },
+            onLeave: () => { devKindTl.pause() },
+            onLeaveBack: () => { devKindTl.pause() }
+        })
+    })
 </script>
 
 <template>
-    <header class="tracking-tighter relative h-dvh w-dvw">
+    <header class="video-container tracking-tighter relative h-dvh w-dvw">
         <video 
             class="absolute -z-10 w-full h-full object-cover pointer-events-none" 
             :src="video"
-            playsinline autoplay loop muted>
+            playsinline autoplay loop muted preload="auto">
         </video>
         <div class="font-primary pt-20 pb-20 grid h-full w-full grid-cols-12 grid-rows-4">
             <div class="col-start-1 col-span-13 row-1 text-center">
@@ -19,11 +68,13 @@
                 </h1>
             </div>
             <div class="text-3xl row-start-2 col-start-4">
-                <div class="text-restrest italic">Creative</div>
+                <div 
+                    ref="kindOfDev"
+                    class="cursor text-restrest"></div>
                 <div class="italic text-rest font-medium">Developer</div>
             </div>
             <div class="flex flex-col text-3xl row-start-3 col-start-9 items-end">
-                <div class="text-restrest italic">...based in</div>
+                <div class="text-restrest">...based in</div>
                 <div class="italic text-dan text-rest font-medium">Denmark</div>
             </div>
             <div class="text-xl pt-10 font-secondary row-start-4 text-center col-start-1 col-end-13">
