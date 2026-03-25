d7fea4c16e7a001c1c6b9312642b81b66ed5dae9
andreialex232 <numauitnuintelegi@gmail.com>
2026-03-26 00:59:44 +0100

    basic project component

---
+++ src/components/Projects.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="pt-20 pb-20 bg-secondary text-primary h-dvh w-dvw font-secondary grid grid-cols-12 ">
+        <div class="col-start-2 col-end-5">
+
+            <!-- project title and description -->
+            <h3 class="font-secondary font-bold text-3xl">
+            The Lionheart Brothers
+            </h3>
+            <p class="text-xl font-secondary mt-2">The Brothers Lionheart is a digital creative project developed during first semester at Erhvervsakademi Sydvest. It is based on the classic play "The Lionheart Brothers" by the renowned author Astrid Lindgren.</p>
+            <div class="mt-10">
+                <p class="font-secondary font-bold text-lg">Team members:</p>
+                <ul class="">
+                    <li>John Doe</li>
+                    <li>Jane Smith</li>
+                    <li>Bob Johnson</li>
+                </ul>
+            </div>
+
+
+            <!-- tech used -->
+            <div class="mt-10">
+                <p class="font-secondary font-bold text-lg">Technologies used:</p>
+                <ul class="">
+                    <li>Vue.js</li>
+                    <li>SCSS</li>
+                    <li>Three.js</li>
+                </ul>
+            </div>
+
+
+            <!-- link -->
+            <div class="mt-10">
+                <p class="font-secondary font-bold text-lg">Project Link:</p>
+                <a href="https://github.com/yourusername/yourproject" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            </div>
+        </div>
+
+
+
+        <div class="relative col-start-5 col-end-13 w-full h-full">
+                <img src="../assets/svgs/tele.svg" alt="">
+                <img class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[63.5%] w-4/5" src="../assets/images/Screenshot 2026-02-21 140438 1.png" alt="">
+        </div>
+    </section>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
     import Hero from '@/components/Hero.vue';
     import About from '@/components/About.vue';
     import Contact from '@/components/Contact.vue';
+    import Projects from '@/components/Projects.vue';
 </script>
 
 <template>
     <Hero></Hero>
     <main>
         <About></About>
+        <Projects></Projects>
         <Contact></Contact>
     </main>
 </template>
