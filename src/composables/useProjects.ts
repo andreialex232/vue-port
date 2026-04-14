0c4d62bf92309265cffb5fabb47006b1ed25f05e
andreialex232 <numauitnuintelegi@gmail.com>
2026-04-15 00:02:22 +0200

    replaced the placeholder text with proper descriptions

--- src/composables/useProjects.ts
+++ src/composables/useProjects.ts
@@ -11,13 +11,15 @@ import ts from "@/assets/svgs/typescript.svg"
 import angular from "@/assets/svgs/angular.svg"
 import scss from "@/assets/svgs/scss.svg"
 import reversedGit from "@/assets/svgs/github-reversed.svg"
+import mongo from "@/assets/svgs/mongo.svg"
+import node from "@/assets/svgs/nodejs.svg"
 
 export const useProjects = () => {
     const projects = ref<Project[]>([
         {
             id: 1,
             name: 'The Brothers Lionheart',
-            description: 'The team and I brought Astrid Lindgren’s The Brothers Lionheart to life by turning the classic play into a sleek, interactive website. We achieved a smooth and professional digital experience that works perfectly on both phones and computers.  We first built designs in Figma and Adobe, making sure the look and feel matched the story’s mood. We then coded the site from scratch using HTML, CSS, and JavaScript, while using GitHub to work together effectively as a team. The result is a modern tribute that combines creative storytelling with solid technical skills.',
+            description: "HTML, CSS, and JavaScript were mainly used by our group to make an excellent interactive website out of The Brothers Lionheart. A rating of B for sustainability in Carbon Rating was attained together with outstanding performance on the desktop, where 99 for Performance, 96 for Accessibility, 100 for Best Practices, and 100 for SEO was earned (Performance 82 on mobile).",
             image: lionheart,
             githubLink: {
                 link: "https://github.com/andreialex232/The-Lionheart-Brothers",
@@ -67,7 +69,7 @@ export const useProjects = () => {
         {
             id: 2,
             name: 'CPM Ridesharing',
-            description: 'The Brothers Lionheart is a digital creative project developed during first semester at Erhvervsakademi Sydvest. It is based on the classic play "The Lionheart Brothers" by the renowned author Astrid Lindgren.',
+            description: 'This project is a professional platform built with Angular, Sass, and TypeScript to streamline ridesharing collaboration and car rentals. By focusing on clean architecture and modern development practices, I achieved a fast, scalable interface that connects drivers with fleet opportunities. The use of TypeScript ensured code reliability, while Sass allowed for a polished, responsive design tailored for a seamless user experience.',
             image: lionheart,
             githubLink: {
                 link: "https://github.com/andreialex232/cpm-ridesharing",
@@ -91,7 +93,7 @@ export const useProjects = () => {
         {
             id: 3,
             name: 'Aswik',
-            description: 'The Brothers Lionheart is a digital creative project developed during first semester at Erhvervsakademi Sydvest. It is based on the classic play "The Lionheart Brothers" by the renowned author Astrid Lindgren.',
+            description: 'I built ASWIK as a personal project to practice connecting a backend to a database while creating a tool for my own Swedish studies. By using NodeJS and MongoDB, I successfully handled how data interacts between the server and the storage, allowing me to save and track my own word lists. I achieved a functional result where I can log my session history and customize how I practice grammar. It’s a straightforward JavaScript application that works as both a coding exercise in data management and a practical way to store and learn new vocabulary.',
             image: lionheart,
             githubLink: {
                 link: "https://github.com/andreialex232/aswik",
@@ -99,12 +101,12 @@ export const useProjects = () => {
             },
             technologies: [
                 {
-                    name: 'HTML',
-                    icon: html
+                    name: 'NodeJS',
+                    icon: node
                 },
                 {
-                    name: 'CSS',
-                    icon: css
+                    name: 'MongoDB',
+                    icon: mongo
                 },
                 {
                     name: 'JavaScript',
