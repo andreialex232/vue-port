e180f07113e40f9e66d947c2d192e1abb0acf9cb
andreialex232 <numauitnuintelegi@gmail.com>
2026-04-18 21:12:52 +0200

    other small projects composable

---
+++ src/composables/useOtherProjects.ts
@@ -0,0 +1,123 @@
+import { ref } from "vue";
+import police_brutality_poster from "@/assets/images/posters/police_comp.png";
+import adobe_ai from "@/assets/svgs/adobe-ai.svg";
+import github_icon from "@/assets/svgs/github-reversed.svg";
+import adobe_ae from "@/assets/svgs/adobe-ae.svg";
+import php from "@/assets/svgs/php.svg";
+import wp from "@/assets/svgs/wordpress.svg";
+import rocket_anim from "@/assets/animations/rocket.mp4";
+import hotel_cms from "@/assets/images/hotel-cms.png";
+
+export const useOtherProjects = () => {
+    const otherProjects = ref<OtherProjects>({
+        posters: [
+            {
+                id: "p1",
+                name: "Police Brutality Awareness Poster",
+                subheading: "The Deadly Pattern",
+                img: police_brutality_poster,
+                technology: {
+                    name: "Adobe Illustrator",
+                    icon: adobe_ai
+                },
+                description: "Infographic poster designed to raise awareness regarding the rise of police brutality in the U.S despire years of calls for reform. It combines statistical data, specific case studies, and policy analysis to argue that systemic failures are preventing progress"
+            },
+            {
+                id: "p2",
+                name: "Marketing Poster For Racing Event",
+                subheading: "Racing 230",
+                img: police_brutality_poster,
+                technology: {
+                    name: "Adobe Illustrator",
+                    icon: adobe_ai
+                },
+                description: "Poster design for a local racing event, highlighting the excitement and thrill of the competition. The design incorporates dynamic imagery, bold typography, and vibrant colors to capture the attention of potential attendees and convey the energy of the event."
+            },
+        ],
+        wordpress: [
+            {
+                id: "w1",
+                name: "WordPress CMS Project",
+                description: "A WordPress project created for a school assignment. the assignment included daily scrum meetings, sprint planning, and retrospectives. the project was built using a custom theme and included features such as custom post types, custom fields. the project was developed using PHP, HTML, CSS",
+                img: hotel_cms,
+                githubLink: {
+                    name: "See Final Project",
+                    url: "https://github.com/Patricia-P-G/HotelCMS",
+                    icon: github_icon,
+                },
+                technology: [{
+                    name: "WordPress",
+                    icon: wp
+                },
+                {
+                    name: "PHP",
+                    icon: php
+                }]
+            }
+        ],
+        animations: [
+            {
+                id: "a1",
+                name: "Rocket To Moon Animation",
+                description: "Animation created for my video cv which was also required in a school project. the animation was created using adobe after effects and adobe illustrator. the purpose is to showcase my skills in motion graphics and video editing, as well as to create a visually engaging section in my video cv",
+                source: rocket_anim,
+                technology: [{
+                    name: "Adobe After Effects",
+                    icon: adobe_ae
+                },
+                {
+                    name: "Adobe Illustrator",
+                    icon: adobe_ai
+                }]
+            }
+        ]
+    })
+
+    return {
+        otherProjects
+    }
+}
+
+// building blocks
+interface GithubLink {
+    name: string;
+    url: string;
+    icon: string;
+}
+
+interface Poster {
+    id: string;
+    name: string;
+    subheading: string;
+    img: string;
+    description: string;
+    technology: technology;
+}
+
+interface WordPressProject {
+    id: string;
+    name: string;
+    description: string;
+    img: string;
+    githubLink: GithubLink;
+    technology: technology[];
+}
+
+interface Animation {
+    id: string;
+    name: string;
+    description: string;
+    source: string;
+    technology: technology[];
+}
+
+interface technology {
+    name: string;
+    icon: string;
+}
+
+export interface OtherProjects {
+    posters: Poster[];
+    wordpress: WordPressProject[];
+    animations: Animation[];
+}
