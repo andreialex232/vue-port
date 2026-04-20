3321ec81667dfee96ac58633534ee742e9cffa91
andreialex232 <numauitnuintelegi@gmail.com>
2026-04-20 06:51:56 +0200

    fixed layout issus at 300px

--- src/components/OtherProjects.vue
+++ src/components/OtherProjects.vue
@@ -44,7 +44,7 @@
                 loading="lazy"
                 :src="poster.img" 
                 :alt="poster.name" 
-                class="w-full sm:h-[400px] lg:h-[250px] xl:h-[300px] 2xl:h-[410px] object-cover rounded-lg shadow-lg transition-transform duration-300 hover:scale-105"
+                class="w-full sm:h-[400px] lg:h-[250px] xl:h-[300px] 2xl:h-[410px] object-cover rounded-lg shadow-2xl transition-transform duration-300 hover:scale-105"
             >
         </div>
     </article>
@@ -69,7 +69,7 @@
                     loading="lazy"
                     :src="poster.img" 
                     :alt="poster.name" 
-                    class="w-full h-[400px] md:h-[250px] object-contain rounded-lg transition-transform duration-300 hover:scale-105"
+                    class="w-full h-[340px] sm:h-[300px] md:h-[250px] object-contain rounded-lg transition-transform duration-300 hover:scale-105"
                 >
             </div>
 
@@ -94,19 +94,29 @@
 
 
 
-<!-- // Animations section - only for mobile screens -->
-
-<div class="anim p-4 px-8 md:px-16 xl:px-24 font-secondary">
-    <div class="max-w-full lg:max-w-[75%] mx-auto">
+<!-- // Animations section -->
+<div class="anim p-4 px-8 md:px-16 lg:px-24 font-secondary">
+    <div class="max-w-full lg:max-w-[75%] mx-auto flex flex-col gap-6 lg:gap-10">
         <article 
             v-for="anim in otherProjects.animations" 
             :key="anim.id"
-            class="bg-secondary text-primary flex flex-col lg:flex-row items-center gap-10 p-10 transition-shadow duration-300 shadow-md hover:shadow-lg lg:h-[500px] xl:h-[480px] overflow-hidden mb-10"
+            class="bg-secondary text-primary flex flex-col lg:flex-row items-center gap-6 lg:gap-10 p-6 lg:p-10 transition-shadow duration-300 shadow-md hover:shadow-lg rounded-xl lg:rounded-none overflow-hidden min-h-fit lg:h-[500px] xl:h-[480px]"
         >
-            <div class="flex-1 flex flex-col justify-between h-full py-2 order-2 lg:order-1">
+            <div class="flex-1 flex flex-col justify-between h-full py-2 order-1 lg:order-1">
                 <div>
-                    <h3 class="text-2xl md:text-4xl lg:text-3xl xl:text-4xl font-bold">{{ anim.name }}</h3>
-                    <p class="text-sm md:text-base lg:text-base 2xl:text-lg mt-4 text-balance">
+                    <h3 class="text-2xl md:text-3xl lg:text-xl xl:text-2xl 2xl:text-3xl font-bold">
+                        {{ anim.name }}
+                    </h3>
+
+                    <div class="w-full mt-4 block lg:hidden">
+                        <video
+                            loading="lazy" muted autoplay loop playsinline
+                            :src="anim.source" 
+                            class="w-full h-auto object-cover"
+                        ></video>
+                    </div>
+                    
+                    <p class="text-sm md:text-base lg:text-base 2xl:text-lg mt-4 text-balance leading-relaxed">
                         {{ anim.description }}
                     </p>
                 </div>
@@ -116,18 +126,18 @@
                     <div class="flex flex-wrap lg:flex-col gap-3 lg:gap-2">
                         <div v-for="tech in anim.technology" :key="tech.name" class="flex gap-3 justify-start items-center">
                             <img loading="lazy" :src="tech.icon" :alt="tech.name" class="w-6 h-6" />
-                            <p class="text-base lg:text-md">{{ tech.name }}</p>
+                            <p class="sm:text-base text-sm lg:text-md">{{ tech.name }}</p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="flex-shrink-0 w-full lg:w-1/2 order-1 lg:order-2">
+            <div class="hidden lg:block flex-shrink-0 w-full lg:w-1/2 order-2">
                 <video
                     loading="lazy" muted autoplay loop playsinline
                     :src="anim.source" 
                     :alt="anim.name" 
-                    class="w-full h-auto lg:h-[380px] object-contain rounded-lg transition-transform duration-300"
+                    class="w-full h-auto lg:h-[380px] object-contain transition-transform duration-300"
                 ></video>
             </div>
         </article>
@@ -135,87 +145,56 @@
 </div>
 
 
-
-<!-- // Wordpress project desktops -->
-<div class="wp hidden sm:block p-4 px-4 md:px-16 xl:px-24 font-secondary">
-    <div class="max-w-full lg:max-w-[75%] mx-auto">
+<!-- wordpress -->
+<div class="wp p-4 px-8 md:px-16 lg:px-24 font-secondary">
+    <div class="max-w-full lg:max-w-[75%] mx-auto flex flex-col gap-6">
         <article 
             v-for="wp in otherProjects.wordpress" 
             :key="wp.id"
-            class="bg-secondary text-primary flex flex-col lg:flex-row items-center gap-6 lg:gap-10 p-6 md:p-10 transition-shadow duration-300 shadow-md hover:shadow-lg min-h-fit xl:h-[480px] lg:h-[500px] overflow-hidden mb-8"
+            class="bg-secondary text-primary flex flex-col lg:flex-row items-center gap-6 lg:gap-10 p-6 lg:p-10 transition-shadow duration-300 shadow-md hover:shadow-lg rounded-xl lg:rounded-none overflow-hidden min-h-fit lg:h-[500px] xl:h-[480px]"
         >
-            <div class="flex-1 flex flex-col justify-between h-full py-2 order-2 lg:order-1">
+            <div class="flex-1 flex flex-col justify-between h-full py-2 order-2 lg:order-1 w-full">
                 <div>
-                    <h3 class="text-2xl md:text-4xl lg:text-3xl xl:text-4xl font-bold">{{ wp.name }}</h3>
+                    <h3 class="text-2xl md:text-3xl lg:text-xl xl:text-2xl 2xl:text-3xl font-bold">
+                        {{ wp.name }}
+                    </h3>
+
+                    <div class="w-full mt-4 block lg:hidden">
+                        <img
+                            loading="lazy"
+                            :src="wp.img"
+                            :alt="wp.name" 
+                            class="w-full h-auto object-cover rounded-lg shadow-2xl transition-transform duration-300"
+                        />
+                    </div>
                     
-                    <p class="text-sm md:text-base lg:text-base 2xl:text-lg mt-2 text-balance">
+                    <p class="text-sm md:text-base lg:text-base 2xl:text-lg mt-4 text-balance">
                         {{ wp.description }}
                     </p>
                 </div>
 
                 <div class="flex flex-col gap-2 mt-6 lg:mt-0">
                     <p class="font-bold text-lg">Technology used:</p>
-                    <div class="flex flex-wrap gap-4 lg:flex-col lg:gap-2">
-                        <div
+                    <div class="flex flex-wrap lg:flex-col gap-3 lg:gap-2">
+                        <div 
                             v-for="tech in wp.technology" 
-                            :key="tech.name"
-                            class="flex gap-3 justify-start items-center">
+                            :key="tech.name" 
+                            class="flex gap-3 justify-start items-center"
+                        >
                             <img loading="lazy" :src="tech.icon" :alt="tech.name" class="w-6 h-6" />
-                            <p class="text-base lg:text-md">{{ tech.name }}</p>
+                            <p class="sm:text-base text-sm lg:text-md">{{ tech.name }}</p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="flex-shrink-0 w-full lg:w-1/2 order-1 lg:order-2">
+            <div class="hidden lg:block flex-shrink-0 w-full lg:w-1/2 order-1 lg:order-2">
                 <img
                     loading="lazy"
                     :src="wp.img"
                     :alt="wp.name" 
-                    class="hover:scale-105 w-full h-48 md:h-64 lg:h-full object-cover rounded-lg shadow-2xl transition-transform duration-300"
+                    class="w-full h-48 md:h-64 lg:h-full object-cover rounded-lg shadow-2xl transition-transform duration-300 hover:scale-105"
                 />
-            </div>
-        </article>
-    </div>
-</div>
-
-
-
-<!-- wordpress project mobile -->
-<div class="wp block sm:hidden p-4 font-secondary">
-    <div class="flex flex-col gap-6">
-        <article 
-            v-for="wp in otherProjects.wordpress" 
-            :key="wp.id"
-            class="bg-secondary text-primary flex flex-col gap-6 rounded-xl p-6 transition-shadow duration-300 shadow-md hover:shadow-lg overflow-hidden"
-        >
-            <h3 class="text-2xl md:text-4xl lg:text-3xl xl:text-4xl font-bold">
-                {{ wp.name }}
-            </h3>
-
-            <div class="w-full">
-                <img
-                    loading="lazy"
-                    :src="wp.img"
-                    :alt="wp.name" 
-                    class="w-full h-auto object-cover rounded-lg shadow-2xl transition-transform duration-300"
-                />
-            </div>
-
-            <p class="text-sm md:text-base lg:text-lg text-balance leading-relaxed">
-                {{ wp.description }}
-            </p>
-
-            <div class="flex flex-col gap-2">
-                <p class="font-bold text-lg">Technology used:</p>
-                <div
-                    v-for="tech in wp.technology" 
-                    :key="tech.name"
-                    class="flex gap-3 justify-start items-center"
-                >
-                    <img loading="lazy" :src="tech.icon" :alt="tech.name" class="w-6 h-6" />
-                    <p class="text-base lg:text-md">{{ tech.name }}</p>
-                </div>
             </div>
         </article>
     </div>
